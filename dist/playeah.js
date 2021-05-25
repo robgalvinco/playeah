f40ce36b4959a86e96403492dbdac60184718888
Robert Galvin <cfh463@zebra.com>
2021-05-25 08:31:10 -0400

    alt dash link

--- dist/playeah.js
+++ dist/playeah.js
@@ -276,26 +276,7 @@ const inject_styles = function(){
                     }                
                 }
 
-                // For alt dashboard link
-                if(typeof(window._alt_dash_links)!="undefined"){
-                    var __FOUND = window._alt_dash_links.findIndex(function(link, index) {
-                    	if(link.courseid == data.course.id)
-                    		return true;
-                    });
-                    if(__FOUND != -1){
-                        $(".top-bar__dashboard-link__anchor").attr("href",window._alt_dash_links[__FOUND].dash_link);
-                    } else {
-                         __FOUND = window._alt_dash_links.findIndex(function(link, index) {
-                        	if(link.courseid == "all")
-                        		return true;
-                        });
-                        if(__FOUND != -1){
-                            $(".top-bar__dashboard-link__anchor").attr("href",window._alt_dash_links[__FOUND].dash_link);
-                        }
-                        
-                    }
-                    
-                }                
+                               
                 
             });
 
